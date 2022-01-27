69625630993b0ba1e2f106a559d313252408d0ea
Spiligia <spiligia@bu.edu>
2022-01-27 16:46:19 -0500

    updated some values for base arduino
    1023bit for non ble arduinos - may use megas

--- CircuitTempControl/CircuitTempControl.ino
+++ CircuitTempControl/CircuitTempControl.ino
@@ -1,24 +1,24 @@
 //Set I/O
 const int INPUT_PIN = A1;
 //set pins to variable note: cannnot be const since they will be changed 
-int OUTPUT_RELAY = D7;
+int OUTPUT_RELAY = D3;
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(1000);
   pinMode(INPUT_PIN,INPUT);
   pinMode(OUTPUT_RELAY,OUTPUT);
   //Resolution specific to arduino
-  analogReadResolution(12);
+  analogReadResolution(10);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(OUTPUT_RELAY, HIGH);
   int sensorReading = analogRead(INPUT_PIN);
-  double voltageTherm = (sensorReading/4095.0) * 3.3;
+  double voltageTherm = (sensorReading/1023.0) * 3.3;// 1023 but to the bit representation of the arduino
   double Rs = 10000; 
   double currentBias = (voltageTherm - 3.3) / -Rs; // currentBias is in A; 
   double resistance = (voltageTherm / currentBias); // resistance is in ohms
@@ -29,7 +29,7 @@ void loop() {
   Serial.print(voltageTherm+p1+tempC);
   Serial.print('\n');
   delay(10); //change to desired sampling rate 
-  if (tempC >= 35)// change to desired temperature 
+  if (tempC >= 33)// change to desired temperature 
   {
     digitalWrite(OUTPUT_RELAY, LOW);
     delay(5000); //Change to desired timer(ms) 
